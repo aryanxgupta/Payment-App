2c792ab853ba3dd1dc29aba9a2888d68e458858f
heyaryan001 <hey.itss.aryan@gmail.com>
2024-08-11 20:57:49 +0530

    Add the package.json

--- frontend/payment-application/src/App.jsx
+++ frontend/payment-application/src/App.jsx
@@ -13,9 +13,8 @@ function App() {
           <Route path='/signup' element={<Signup />} />
           <Route path='/signin' element={<Signin />} />
           <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/sendmoney' element={<SendMoney />} />
-          <Route path="/" element={<Navigate to="/signup" />} />
-          <Route path="*" element={<Navigate to="/signup" />} />
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="*" element={<Navigate to="/signin" />} />
         </Routes>
       </BrowserRouter>
     </div>
