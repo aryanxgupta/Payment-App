6f7aedbff1686aea1153202fbda58d241999bc86
heyaryan001 <hey.itss.aryan@gmail.com>
2024-08-11 21:49:42 +0530

    modify package

--- frontend/payment-application/vite.config.js
+++ frontend/payment-application/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Payment-App/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-  }, 
-  base: './'
+  }
 })
